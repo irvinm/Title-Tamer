f918587c6a699035ef74652662daa30201246b1f
Mike Irvin <irvinm@yahoo.com>
2024-11-10 16:36:51 -0500

    Add SVG files to XPI

--- MakeXPI.bat
+++ MakeXPI.bat
@@ -1,3 +1,3 @@
 del TitleTamer.xpi
-"C:\Program Files\WinRAR\winrar.exe" a TitleTamer.zip manifest.json *.js *.html *.css
+"C:\Program Files\WinRAR\winrar.exe" a TitleTamer.zip manifest.json *.js *.html *.css *.svg
 ren TitleTamer.zip TitleTamer.xpi
