c9a8e67fc1650f3c0a1106fe70baa9ace3202e0c
Mike Irvin <irvinm@yahoo.com>
2024-11-12 06:45:16 -0500

    Add PNG files to make items

--- MakeXPI.bat
+++ MakeXPI.bat
@@ -1,3 +1,3 @@
 del TitleTamer.xpi
-"C:\Program Files\WinRAR\winrar.exe" a TitleTamer.zip manifest.json *.js *.html *.css *.svg
+"C:\Program Files\WinRAR\winrar.exe" a TitleTamer.zip manifest.json *.js *.html *.css *.svg *.png
 ren TitleTamer.zip TitleTamer.xpi
